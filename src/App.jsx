a6ea43c48d550a12a59cc294426e23d5e4a999ab
Ciler K <cilerk@icloud.com>
2024-09-14 21:28:56 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar';
+
+function App() {
+  
+  return (
+    <div className='text-2xl text-green-600'>
+      <Navbar />
+    </div>
+    )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (<div>
+     <div>upper  navbar </div>
+     <div>
+      <a href="#"><img src="Logo" alt="Logo" /></a>
+     </div>
+<div>lower navbar</div>
+
+
+    </div>
+  )
+
+}
+
+export default Navbar
+
+// sana bir sey gostereyim.. devam
